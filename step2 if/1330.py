5aee019f6107a989ea8af2cde0413c52dfee343b
hysKim1 <42089407+hysKim1@users.noreply.github.com>
2021-06-18 18:04:01 +0900

    20210618

--- step2 if/1330.py
+++
@@ -1,8 +0,0 @@
-#1330	두 수 비교하기
-a,b =map(int, input().split())
-if a>b:
-    print('>')
-elif a<b:
-    print('<')
-else:
-    print('==')
--- step2 if/2753.py
+++
@@ -1,6 +0,0 @@
-#2753	윤년
-year = int(input())
-if year %4==0 and (year%100!=0| year %400==0) :
-    print(1)
-else:
-    print(0)
