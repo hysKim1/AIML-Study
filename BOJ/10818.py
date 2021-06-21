3385906ff4f1fab4265e5a83163a33634379c566
hysKim1 <42089407+hysKim1@users.noreply.github.com>
2021-06-21 22:50:07 +0900

    Revert "20210621"
    This reverts commit 4b20131d96d3da63e56717f0db5be0b87377d3f9.

--- BOJ/10818.py
+++
@@ -1,4 +0,0 @@
-#10818   최소, 최대
-N = int(input())
-arr = list(map(int, input().split()))
-print(min(arr), max(arr))
--- BOJ/2562.py
+++
@@ -1,5 +0,0 @@
-#2562    최댓값
-arr=[]
-for i in range(9):
-    arr.append(int(input()))
-print(str(max(arr))+'\n'+str(arr.index(max(arr))+1))
