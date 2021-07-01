bb8115e921d779700b372ef2885a1858855ae2a4
hysKim1 <42089407+hysKim1@users.noreply.github.com>
2021-07-02 00:04:49 +0900

    20210701

---
+++ BOJ/step8 math1/2775.py
@@ -0,0 +1,13 @@
+#2775  부녀회장이 될테야
+
+for _ in range(int(input())):
+    k = int(input())
+    n = int(input())
+
+    people = [i for i in range(n + 1)]
+
+    for i in range(k):
+        for j in range(1, n + 1):
+            people[j] = people[j] + people[j - 1]
+
+    print(people[-1])
