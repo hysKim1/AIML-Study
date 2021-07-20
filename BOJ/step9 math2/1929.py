aa5048021fa2d99af9d5ec5a516dbe820b97acca
hysKim1 <42089407+hysKim1@users.noreply.github.com>
2021-07-20 17:56:58 +0900

    20210720

--- BOJ/step9 math2/1929.py
+++ BOJ/step9 math2/1929.py
@@ -1,4 +1,13 @@
 #1929 소수 구하기
+#1 short ver.
+M,N=map(int,input().split())
+*s,=range(N+1)
+
+for i in s[2:]:
+    if s[i]:
+        s[2*i::i]=[0]*(N//i-1)
+        if(i>=M):print(i)
+#2            
 n1, n2= map(int, input().split())
 li = [1]*(n2+1)
 for i in range(2, int((n2+1)**0.5) + 1):
